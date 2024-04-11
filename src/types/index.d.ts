fe077c7ff4d0ff86a69bf306b85f20dfccba8512
Alejandro Sandí <alesf22@gmail.com>
2024-04-10 20:59:21 -0600

    chore: add global types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,29 @@ import { ReactNode } from 'react';
 export type LayoutProps = {
   children: ReactNode;
 };
+
+export type SuccessState<T> = {
+  message: string;
+  data: T;
+};
+
+export type ErrorState = {
+  message: string;
+  type?: string;
+};
+
+export type FormErrorState = {
+  error?: ErrorState | null;
+};
+
+export type QueryResponse<T> = {
+  success: SuccessState<T> | null;
+  error: ErrorState;
+};
+
+export interface MutationResponse extends FormErrorState {
+  success?: {
+    message: string;
+    state: boolean;
+  } | null;
+}
