5ae11e032605bbd02eea90b3a725fe939f370cc7
Alejandro Sandí <alesf22@gmail.com>
2024-04-14 18:06:01 -0600

    feat: create send support email service

---
+++ src/services/users/mutations/support-email.ts
@@ -0,0 +1,37 @@
+import { SERVER_BASE_API } from '@/lib/constants';
+import { handleError } from '@/lib/utils';
+import { MutationResponse } from '@/types';
+
+export async function supportEmail(
+  prevState: MutationResponse,
+  formData: FormData,
+): Promise<MutationResponse> {
+  try {
+    const rawFormData = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      reason: formData.get('reason'),
+      note: formData.get('note'),
+    };
+
+    const response = await fetch(`${SERVER_BASE_API}/users/support`, {
+      method: 'POST',
+      body: JSON.stringify(rawFormData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    if (!response.ok) return handleError(data.message);
+
+    return {
+      success: {
+        message: 'Email sent successfully',
+      },
+      error: null,
+    };
+  } catch (error) {
+    return handleError('Something went wrong');
+  }
+}
