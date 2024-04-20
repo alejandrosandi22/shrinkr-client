d4f1f5330820a5e4a6e7f3a34dd4bc5d528662df
Alejandro Sandí <alesf22@gmail.com>
2024-04-19 21:41:51 -0600

    feat: center form on screen

--- src/app/auth/recovery/page.tsx
+++ src/app/auth/recovery/page.tsx
@@ -2,7 +2,7 @@ import RecoveryEmailForm from '@/components/auth/recovery-email-form';
 
 export default function Recovery() {
   return (
-    <main className='mx-auto w-full max-w-md p-6'>
+    <main className='absolute left-[50%] top-[50%] mx-auto w-full max-w-md translate-x-[-50%] translate-y-[-50%] p-6'>
       <RecoveryEmailForm />
     </main>
   );
