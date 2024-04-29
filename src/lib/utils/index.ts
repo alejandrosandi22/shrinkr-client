e27850b607324b1a542664d38e136dc1ac958794
Alejandro Sandí <alesf22@gmail.com>
2024-04-29 10:17:02 -0600

    feat: create referrer parser function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,6 @@
 import cn from '@/lib/utils/cn';
 import getAccessToken from '@/lib/utils/getAccessToken';
 import handleError from '@/lib/utils/handleError';
+import referrerParser from '@/lib/utils/referrerParser';
 
-export { cn, getAccessToken, handleError };
+export { cn, getAccessToken, handleError, referrerParser };
