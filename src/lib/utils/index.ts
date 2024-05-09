7938d4d5a1ad3261c729bf587de87da40e85196a
Alejandro Sandí <alesf22@gmail.com>
2024-05-09 10:59:45 -0600

    feat: update horizontal bar chart

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,6 @@
 import cn from '@/lib/utils/cn';
+import formatLabel from '@/lib/utils/formatLabel';
 import getAccessToken from '@/lib/utils/getAccessToken';
 import handleError from '@/lib/utils/handleError';
 
-export { cn, getAccessToken, handleError };
+export { cn, formatLabel, getAccessToken, handleError };
